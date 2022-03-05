adb676a0edda1bb065b861f49ba16bd70068a6b7
spzhu <cangcang333@163.com>
2022-03-06 00:06:39 +0800

    add test code and optimise operator= code

--- 01_AssignmentOperator.cpp
+++ 01_AssignmentOperator.cpp
@@ -2,7 +2,6 @@
 // Created by spzhu on 2022/3/2.
 //
 
-
 // 题1：赋值运算符函数
 // 题目：如下为类型 CMyString 的声明，请为该类型添加赋值运算符函数
 
@@ -47,6 +46,21 @@ CMyString::~CMyString() {
     delete []m_pData;
 }
 
+// 赋值运算符实现 better option
+CMyString& CMyString::operator=(const CMyString &str) {
+    if (this != &str)
+    {
+        CMyString strTemp(str);
+
+        char *pTemp = strTemp.m_pData;
+        strTemp.m_pData = m_pData;
+        m_pData = pTemp;
+    }
+
+    return *this;
+}
+
+/*
 CMyString& CMyString::operator=(const CMyString& str)
 {
     if (this == &str)
@@ -60,6 +74,7 @@ CMyString& CMyString::operator=(const CMyString& str)
 
     return *this;
 }
+*/
 
 void CMyString::Print() {
     printf("%s\n", m_pData);
@@ -70,12 +85,53 @@ void Test1()
     printf("Test1 begins:\n");
     char *text = "hi";
     CMyString str1(text);
+    CMyString str2;
+    str2 = str1;
+
+    printf("The expected result is %s\n", text);
+    printf("The actual result is ");
+    str2.Print();
+    printf("\n");
+}
+
+// 赋值给自己
+void Test2()
+{
+    printf("Test2 begins:\n");
+    char *text = "hi";
+    CMyString str1(text);
+    str1 = str1;
+    printf("The expected result is %s\n", text);
+    printf("The actual result is ");
     str1.Print();
+    printf("\n");
+}
+
+// 连续赋值
+void Test3()
+{
+    printf("Test3 begins:\n");
+    char *text = "hi";
+    CMyString str1(text);
+    CMyString str2, str3;
+    str3 = str2 = str1;
+
+    printf("The expected result is %s\n", text);
+    printf("The actual result is ");
+    str2.Print();
+    printf("\n");
+
+    printf("The expected result is %s\n", text);
+    printf("The actual result is ");
+    str3.Print();
+    printf("\n");
 }
 
 int main(int argc, char *argv[])
 {
     Test1();
+    Test2();
+    Test3();
 
     return 0;
 }
