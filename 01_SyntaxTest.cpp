b3b307b64cc79e7b6f308e663eaa18203c89597b
spzhu <cangcang333@163.com>
2022-04-20 20:59:22 +0800

    add 03 init code

--- 01_SyntaxTest.cpp
+++ 01_SyntaxTest.cpp
@@ -32,6 +32,11 @@ public:
     void Print() { std::cout << value << std::endl; }
 };
 
+int GetSize(int data[])
+{
+    return sizeof(data);
+}
+
 int main(int argc, char *argv[])
 {
     // 空类型占用多少内存由编译器决定，在此，每个空类型的实例占用 1 字节的空间
@@ -45,6 +50,18 @@ int main(int argc, char *argv[])
 
     A c(20);
     c.Print();
+
+
+    int data1[] = {1, 2, 3, 4, 5};
+    int size1 = sizeof(data1);
+
+    int *data2 = data1;
+    int size2 = sizeof(data2);
+
+    int size3 = GetSize(data1);
+    printf("size1: %d, size2: %d, size3: %d\n", size1, size2, size3);
+
+
 
     std::cout << "... end ..." << std::endl;
 
